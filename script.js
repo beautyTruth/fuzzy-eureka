4358e38eb6b9cd95c6b6c0fa28365d80fb33a9ef
Phil <happy.melodist@gmail.com>
2021-09-27 22:02:26 -0500

    complete section 3

--- script.js
+++ script.js
@@ -93,3 +93,16 @@ const darkKnight = () => {
 
 darkKnight();
 // end slideshow script
+
+// section 3 script
+const section3Content = document.querySelector(".section-3-content");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.pageYOffset + window.innerHeight >=
+    section3Content.offsetTop + section3Content.offsetHeight / 2
+  ) {
+    section3Content.classList.add("change");
+  }
+});
+// end of section 3 script
